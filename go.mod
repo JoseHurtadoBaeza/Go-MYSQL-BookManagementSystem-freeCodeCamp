60c808d613d14f0bb64bed5e919e2b4cce013904
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2023-09-28 05:58:50 +0200

    main.go: Implemented, book.go: Implemented, app.go:Implemented, utils.go: Implemented, book-controller.go: Developing

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/JoseHurtadoBaeza/Go-MySQL-BookManagementSystem-freeCodeCamp
 
 go 1.21.1
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	gorm.io/gorm v1.25.4
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -8,10 +8,14 @@ var (
 
 func Connect() {
 
-	d, err := gorm.Open("mysql", "root:1234/simplerest?") // Gorm always let us using postgresql or sqlite
+	d, err := gorm.Open("mysql", "root:1234/simplerest?charset=utf8&parseTime=True&loc=Local") // Gorm always let us using postgresql or sqlite
 	if err != nil {
 		panic(err)
 	}
 	db = d
 
+}
+
+func GetDB() *gorm.DB {
+	return db
 }
