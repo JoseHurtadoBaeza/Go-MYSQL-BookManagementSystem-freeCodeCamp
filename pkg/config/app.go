fc6920e41cb64c7b23304786d7ed600150133667
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2023-09-29 05:52:38 +0200

    Corrected the connection to the database

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -13,7 +13,7 @@ func Connect() {
 
 	// Gorm always let us using postgresql, sqlite or sqlserver
 
-	dsn := "jose:1234@tcp(127.0.0.1:3306)/simplerest?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:1234@tcp(127.0.0.1:3306)/simplerest?charset=utf8mb4&parseTime=True&loc=Local"
 
 	d, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
