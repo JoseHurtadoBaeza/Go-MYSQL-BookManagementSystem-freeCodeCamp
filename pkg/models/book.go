68872c3358918f09c3bfc6cb528bbcdfe6cb8b3e
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2023-09-29 17:27:36 +0200

    Adding the left change mentioned in the last commit

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -40,7 +40,7 @@ func GetAllBooks() []Book {
 func GetBookById(Id int64) (*Book, *gorm.DB) {
 
 	var getBook Book
-	db := db.Where("ID=?", Id).Find(&getBook)
+	db := db.Where("ID = ?", Id).Find(&getBook)
 	return &getBook, db
 
 }
@@ -48,7 +48,7 @@ func GetBookById(Id int64) (*Book, *gorm.DB) {
 func DeleteBook(Id int64) Book {
 
 	var book Book
-	db.Where("ID=?", Id).Delete(book)
+	db.Where("ID = ?", Id).Delete(&book)
 	return book
 
 }
